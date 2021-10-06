8f5fa2816832854619a7a92c2a9c61c99d3729c6
Cody Hodge <hodgec@uleth.ca>
2021-10-06 00:36:45 -0600

    table class

---
+++ include/Table.h
@@ -0,0 +1,12 @@
+/**
+ * @author Cody Hodge <hodgec@uleth.ca>
+ * @date 2021-9-19
+ */
+#ifndef TABLE_H
+#define TABLE_H
+
+
+class Table {
+
+};
+#endif
--- src/project/main.cpp
+++ src/project/main.cpp
@@ -2,8 +2,15 @@
 #include <string>
 
 int main () {
-std::string input;
-std::cin >> input;
+std::string input, input1;
+std::cout << "Enter PLaintext: \n";
+std::cin >> input1;
+
+for (int i = 0; i < 16; i++) {
+  input = input + input1[i];
+}
+
+std::cout << input;
 
 char c;
 std::cin >> c;
